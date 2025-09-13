db4849209f611adf141f83b835924f1dc42225b5
wildanjr19 <wildansekolah11@gmail.com>
2025-09-13 13:08:29 +0700

    add Class

--- RoPE/rope.py
+++ RoPE/rope.py
@@ -18,3 +18,30 @@ def rotate_half(x: torch.Tensor) -> torch.Tensor:
     # gabungkan pada dimensi terakhir
     # reshape, pastikan kembali ke bentuk semula
     return torch.stack((-x_ganjil, x_genap), dim=-1).reshape_as(x)
+
+
+class RotaryEmbedding(nn.Module):
+    """
+    RoPE dengan cos sin
+    Args:
+        dim (int): dimensi yang akan dilakukann RoPE (harus genap agar bisa dibagi 2)
+        base (float) : nilai theta default/base = 10000 
+    """
+    def __init__(self, dim: int, base: float = 10000):
+        super().__init__()
+        # pastikan dim genap
+        if dim % 2 != 0:
+            raise ValueError("Dimensi harus genap")
+        
+        self.dim = dim
+        # frekuensi invers
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
+        # register inv_freq agar tidak trainable
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+
+        # caching seq_len
+        self._seq_len_cached = 0
+
+        # register buffer cos sin
+        self.register_buffer("cos_cached", torch.empty(1, 0, 1, dim), persistent=False)
+        self.register_buffer("sin_cached", torch.empty(1, 0, 1, dim), persistent=False)
